7c915fe1dbe4dd5a8c3ab3f5977d7ddd7d765a29
flaymond100 <61448981+flaymond100@users.noreply.github.com>
2021-01-15 10:34:28 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function openPlayer() {
+    var x = document.getElementById("playerFrame");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+
+
